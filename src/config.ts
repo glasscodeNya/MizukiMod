023776fc73818c455f4c31bad0a7818d8a95f29a
GlassCode <zhangjiajun0714@outlook.com>
2026-04-06 10:57:21 +0800

    Part2

--- src/config.ts
+++ src/config.ts
@@ -227,7 +227,7 @@ export const navBarConfig: NavBarConfig = {
 		},
 		{
 			name: "Bilibili",
-			url: "https://space.bilibili.com/701864046",
+			url: "https://space.bilibili.com/483804483",
 			external: true,
 			icon: "fa7-brands:bilibili",
 		},
@@ -248,8 +248,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "まつざか ゆき",
-	bio: "世界は大きい、君は行かなければならない",
+	name: "GlassCode", // 显示在侧边栏个人资料组件中的名字
+	bio: "Tech and ACG?",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
@@ -258,12 +258,12 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "Bilibili",
 			icon: "fa7-brands:bilibili",
-			url: "https://space.bilibili.com/701864046",
+			url: "https://space.bilibili.com/483804483",
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/matsuzaka-yuki",
+			url: "https://github.com/glasscodeNya",
 		},
 	],
 };
